119620582eb72d846f575b435810fda4072727af
Neto Sousa <netosousa29@yahoo.com>
2022-08-09 17:25:39 -0300

    Colocando minha informações na HomeHero

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,4 +1,4 @@
-import { Container, TextContainer } from './styles';
+import { Container, TextContainer, InfosContainer, CodeItem } from './styles';
 import logo from "../../assets/NewLogo2.png"; 
 
 function HomeHero() {
@@ -11,6 +11,30 @@ function HomeHero() {
                     <h1>Olá</h1>
                     <h2>Me chamo Neto</h2>
                 </TextContainer>
+                <InfosContainer>
+                    <CodeItem>
+                        <span className='coment'>//Minha apresentação</span>
+                        <span className='purple'>Infos</span> {'\u007B'}
+                        <div>
+                            Nome: <span className='blue'>Neto,</span>
+                        </div>
+                        <div>
+                            Sobrenome: <span className='blue'>Sousa</span>
+                        </div>
+                        {'\u007B'}
+                    </CodeItem>
+
+                    <CodeItem>
+                        <span className='purple'>Cargo</span> {'\u007B'}
+                        <div>
+                            Função: <span className='blue'>Dev Front-end,</span>
+                        </div>
+                        <div>
+                            Empresa: <span className='blue'>Cidade Alta</span>
+                        </div>
+                        {'\u007B'}
+                    </CodeItem>
+                </InfosContainer>
             </div>
         </Container>
     );
--- src/components/HomeHero/styles.ts
+++ src/components/HomeHero/styles.ts
@@ -11,7 +11,22 @@ export const Container = styled.section`
   > img {
     width: 40rem;
     flex: 1;
+    margin-left: 10rem;
   }
+
+  > div {
+    flex: 4;
+  }
+
+  @media (max-width: 1450px) {
+    > img {
+      width: 30rem;
+    }
+    > div {
+      flex: 1;
+    }
+  }
+
   
   @media (max-width: 1000px) {
     > img {
@@ -35,6 +50,7 @@ export const TextContainer = styled.section`
     font-weight: 400;
     color: ${({ theme }) => theme.secondary};
   }
+
   @media (max-width: 1450px) {
     h1 {
       font-size: 6rem;
@@ -43,6 +59,7 @@ export const TextContainer = styled.section`
       font-size: 3rem;
     }
   }
+  
   @media (max-width: 1000px) {
     h1 {
       font-size: 4rem;
@@ -50,5 +67,50 @@ export const TextContainer = styled.section`
     h2 {
       font-size: 2rem;
     }
+  }
+`;
+
+export const InfosContainer = styled.section`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const CodeItem = styled.pre`
+  background: ${({ theme }) => theme.gradient};
+  padding: 2rem;
+  font-family: 'JetBrains Mono', monospace;
+  font-weight: 300;
+  color: #fff;
+  width: 24rem;
+  align-self: flex-start;
+  transition: 1s;
+
+  &:hover {
+    filter: brightness(1.2);
+  }
+
+  &:last-child {
+    align-self: flex-end;
+  }
+
+  > div {
+    margin: 0.2rem 0;
+    margin-left: 1rem;
+  }
+
+  span.purple {
+    color: #c38cdd;
+  }
+
+  span.blue {
+    color: #7ac7e3
+  }
+
+  span.coment {
+    color: ${({ theme }) => theme.text};
+    margin-bottom: 1rem;
+    display: block;
   }
 `;
