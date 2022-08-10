3885d7da5dd2a72faf0d48ffe7ac599056c14c02
Neto Sousa <netosousa29@yahoo.com>
2022-08-10 10:59:02 -0300

    Personalizando progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components';
+import NextNprogress from 'nextjs-progressbar';
 
 import theme from '../styles/theme';
 import Home from './index'
@@ -7,6 +8,14 @@ import GlobalStyles from '../styles/global';
 function MyApp() {
   return (
     <ThemeProvider theme={theme}>
+       <NextNprogress
+        color={theme.primary}
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow
+      />
+
       <Home />
       <GlobalStyles />
     </ThemeProvider>
